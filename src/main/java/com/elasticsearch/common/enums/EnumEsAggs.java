295eef272e40ec3fcebba8e4338ef67e696cf6fe
memoryFu <fuh@1000video.com.cn>
2020-09-19 17:34:12 +0800

    代码格式优化.

--- src/main/java/com/elasticsearch/common/enums/EnumEsAggs.java
+++ src/main/java/com/elasticsearch/common/enums/EnumEsAggs.java
@@ -1,22 +1,23 @@
 package com.elasticsearch.common.enums;
 
 public enum EnumEsAggs {
-	
-	GROUPBY("groupBy"),// 分组统计
-	HAVING("having"),// 与GROUPBY同时使用，控制返回组数
-	LIMIT("limit"),// 与GROUPBY同时使用，控制组内返回数据条数
-	SORT("sort"),// 与GROUPBY同时使用，控制组内排序，支持desc | asc
-	GROUPBYDATE("groupByDate"),// 按时间分组统计、支持year、quarter、month、week、day、hour、minute、second、millisecond
-	SIZE("size");// 与GROUPBY同时使用，控制返回组数
-	
-	private String opt;
-
-	private EnumEsAggs(String opt) {
-		this.opt = opt;
-	}
-
-	public String getOpt(){
-		return this.opt;
-	}
-
+    
+    GROUPBY("groupBy"),// 分组统计
+    HAVING("having"),// 与GROUPBY同时使用，控制返回组数
+    LIMIT("limit"),// 与GROUPBY同时使用，控制组内返回数据条数
+    SORT("sort"),// 与GROUPBY同时使用，控制组内排序，支持desc | asc
+    SIZE("size"),// 与GROUPBY同时使用，控制返回组数
+    GROUPBYDATE(
+        "groupByDate");// 按时间分组统计、支持year、quarter、month、week、day、hour、minute、second、millisecond
+    
+    private String opt;
+    
+    private EnumEsAggs(String opt) {
+        this.opt = opt;
+    }
+    
+    public String getOpt() {
+        return this.opt;
+    }
+    
 }
